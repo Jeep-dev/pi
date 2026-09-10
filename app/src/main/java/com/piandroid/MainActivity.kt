1e98b114a53c47fb73d54c487260da6e9b3cd637
Jeep-dev <213277512+Jeep-dev@users.noreply.github.com>
2026-09-10 11:15:17 +0800

    Fix Compose inset API import

--- app/src/main/java/com/piandroid/MainActivity.kt
+++ app/src/main/java/com/piandroid/MainActivity.kt
@@ -31,7 +31,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.horizontalScroll
-import androidx.compose.foundation.layout.getBottom
 import androidx.compose.foundation.layout.ime
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.navigationBarsPadding
