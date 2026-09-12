4c60c7ce5b75f18947bb3f39a6d4de0fdc3b04e2
Jeep-dev <junqizhu3@gmail.com>
2026-09-12 22:55:55 +0800

    Bump Pi Android to v5.19.15

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.piandroid"
         minSdk = 26
         targetSdk = 35
-        versionCode = 113
-        versionName = "5.19.14"
+        versionCode = 114
+        versionName = "5.19.15"
     }
 
     signingConfigs {
